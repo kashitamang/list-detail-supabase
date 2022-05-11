bc6af1ce153f9ce034cdcef8ba69dc4950635717
KASHI TAMANG <tamang.kashi@gmail.com>
2022-05-11 12:59:26 -0700

    added script tags, supabase URL, KEY and client variable

---
+++ fetch-util.js
@@ -0,0 +1,5 @@
+const SUPABASE_URL = 'https://kxmmrwriuitalwrkjfpg.supabase.co';
+const SUPABASE_KEY =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt4bW1yd3JpdWl0YWx3cmtqZnBnIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTIyOTc2MTgsImV4cCI6MTk2Nzg3MzYxOH0.FifnX44R83DRNIIvOYNAWTyoqlbl45PDPn8n0duFB7o';
+
+const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
